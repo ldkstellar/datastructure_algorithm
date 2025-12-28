1d5ecb42876ad3b71f7f8c9363be2e51428c7def
leedongkyu <leedongkyu0225@gmail.com>
2025-12-28 13:53:14 +0900

    feat: remove 함수 완성

--- linked-list/linked_list.cpp
+++ linked-list/linked_list.cpp
@@ -6,39 +6,55 @@ public:
   int data;
   Node *next;
 };
-
-int main() {
-
-  int A[] = {3, 5, 7, 10, 15};
-
+Node *create(int A[], int size) {
   Node *head = new Node; // 새 노드생성
 
   Node *temp;
   Node *last;
 
-  head->data = A[0];
+  head->data = A[0]; // 배열에  첫번째 값 삽입
   head->next = nullptr;
-  last = head;
+  last = head; // 초기엔는 마지막이 head이다
 
   // Create a Linked List
-  for (int i = 1; i < sizeof(A) / sizeof(A[0]); i++) {
+  for (int i = 1; i < size; i++) {
 
     temp = new Node;
 
     temp->data = A[i];
     temp->next = nullptr;
 
-    last->next = temp; // head로 숨기고 있어
+    last->next = temp; // head로 숨기고 있어 last
     last = temp;       // head 이제 last가 아니야
   }
 
   // Display Linked List
   Node *p = head;
 
+  return p;
+}
+
+void remove(Node *p) {
+  Node *node = p;
+  while (node->next != nullptr) {
+    Node *temp = node->next;
+    delete node;
+    node = temp;
+  }
+}
+
+int main() {
+
+  int A[] = {3, 5, 7, 10, 15};
+
+  Node *p = create(A, 5);
+
   while (p != nullptr) {
     cout << p->data << " -> " << flush;
     p = p->next;
   }
+
+  remove(p);
 
   return 0;
 }
